4ea94de59cac5fae7aecb4c8a4c80c9e215df121
Ayina Anyachebelu <ayinaanyachebelu@gmail.com>
2023-08-05 23:32:07 -0400

    edited temp file

--- temp.py
+++ temp.py
@@ -3,3 +3,11 @@ import torch
 print(torch.cuda.is_available())
 
 print("device is ", torch.device)
+
+
+# Create a tensor on CPU
+x = torch.randn(100, 100)
+
+# Move the tensor to GPU
+x = x.to('cuda')
+print("done")
