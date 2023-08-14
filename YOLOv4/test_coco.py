8c6812d700d68b6a56d2ab314a717ee4b244208c
Ayina Anyachebelu <ayinaanyachebelu@gmail.com>
2023-08-13 23:57:15 -0500

    made changes to test_coco.py

--- YOLOv4/test_coco.py
+++ YOLOv4/test_coco.py
@@ -257,7 +257,7 @@ def test(data,
     # Save JSON
     if save_json and len(jdict):
         w = Path(weights[0] if isinstance(weights, list) else weights).stem if weights is not None else ''  # weights
-        anno_json = glob.glob('/home/ayina/MscThesis/DCW/datasets/Dataset_final/DATA_0_COCO_format/annotations/instances_test*.json')[0]  # annotations json
+        anno_json = glob.glob('../coco/annotations/instances_test*.json')[0]  # annotations json
         pred_json = str(save_dir / f"{w}_predictions.json")  # predictions json
         print('\nEvaluating pycocotools mAP... saving %s...' % pred_json)
         with open(pred_json, 'w') as f:
