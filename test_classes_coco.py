a86b993810cadc4e553a1ac1a55eaa9293397006
Ayina Anyachebelu <ayinaanyachebelu@gmail.com>
2023-08-24 22:12:39 -0400

    small edit

--- test_classes_coco.py
+++ test_classes_coco.py
@@ -1,7 +1,7 @@
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
 
-ann_path = '/home/ayina/MscThesis/DCW/datasets/Dataset_final/DATA_0_COCO_format/annotations/instances_test.json'
+ann_path = '/home/ayina/MscThesis/DCW/datasets/Dataset_final/DATA_0_COCO_format/annotations/instances_test2017.json'
 pred_path = '/home/ayina/MscThesis/DCW/YOLOv4/runs/test/exp9/best_predictions.json'
 
 ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
