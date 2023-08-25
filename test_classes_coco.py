a041d45e796134511ca2271f2568268ee1f6f157
Ayina Anyachebelu <ayinaanyachebelu@gmail.com>
2023-08-24 22:47:59 -0400

    made test_coco_classes script create pandas dataframe of results

--- test_classes_coco.py
+++ test_classes_coco.py
@@ -1,22 +1,52 @@
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
+import pandas as pd
 
 ann_path = '/home/ayina/MscThesis/DCW/datasets/Dataset_final/DATA_0_COCO_format/annotations/instances_test2017.json'
 pred_path = '/home/ayina/MscThesis/DCW/YOLOv4/runs/test/exp9/best_predictions.json'
 
+columns = ['Category', 'AP50', 'AP@[0.50:0.95]', 'AR@[0.50:0.95]']
+results = []
+
 ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+cats = [
+    "Waterhemp",
+    "MorningGlory",
+    "Purslane",
+    "SpottedSpurge",
+    "Carpetweed",
+    "Ragweed",
+    "Eclipta",
+    "PricklySida",
+    "PalmerAmaranth",
+    "Sicklepod",
+    "Goosegrass",
+    "CutleafGroundcherry"
+]
 
 
 def main():
-    for cat in ids:
+    for id in ids:
+        num = id - 1
+        cat = cats[num]
+
         Gt = COCO(ann_path)
         dets = Gt.loadRes(pred_path)
 
         coco_eval = COCOeval(Gt, dets, "bbox")
-        coco_eval.params.catIds = cat
+        coco_eval.params.catIds = id
         coco_eval.evaluate()
         coco_eval.accumulate()
         coco_eval.summarize()
+
+        ap_50 = round(coco_eval.stats[1], 3)
+        ap_5095 = round(coco_eval.stats[0], 3)
+        aR = round(coco_eval.stats[7], 3)
+
+        results.append([cat, ap_50, ap_5095, aR])
+
+    results_df = pd.DataFrame(results, columns=columns)
+    print(results_df.to_string())
 
 
 if __name__ == '__main__':
