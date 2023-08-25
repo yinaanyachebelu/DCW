0ae979e1bbafa9f3a158b7f0422b3979820eaac8
Ayina Anyachebelu <ayinaanyachebelu@gmail.com>
2023-08-24 22:07:20 -0400

    created script to test by class

---
+++ test_classes_coco.py
@@ -0,0 +1,17 @@
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+
+ann_path = '/home/ayina/MscThesis/DCW/datasets/Dataset_final/DATA_0_COCO_format/annotations/instances_test.json'
+pred_path = '/home/ayina/MscThesis/DCW/YOLOv4/runs/test/exp9/best_predictions.json'
+
+ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+for cat in ids:
+    Gt = COCO(ann_path)
+    dets = Gt.loadRes(pred_path)
+
+    coco_eval = COCOeval(Gt, dets, "bbox")
+    coco_eval.params.catIds = cat
+    coco_eval.evaluate()
+    coco_eval.accumulate()
+    coco_eval.summarize()
