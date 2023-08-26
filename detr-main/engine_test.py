6e75dcd7207177395d4d9cb78067bb9c3f488866
Ayina Anyachebelu <ayinaanyachebelu@gmail.com>
2023-08-26 19:10:45 -0400

    troubleshooting

--- detr-main/engine_test.py
+++ detr-main/engine_test.py
@@ -116,7 +116,7 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
             results = postprocessors['segm'](results, outputs, orig_target_sizes, target_sizes)
 
         for i in results:
-            for v, k in i:
+            for v, k in i.items():
                 k = k.cpu().data.numpy()
 
         res = {target['image_id'].item(): output for target, output in zip(targets, results)}
