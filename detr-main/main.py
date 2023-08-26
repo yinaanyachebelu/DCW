8d9f2a04d1cc17bbb869af5990f4020e3a5c7d4c
Ayina Anyachebelu <ayinaanyachebelu@gmail.com>
2023-08-26 05:04:14 -0400

    small edit

--- detr-main/main.py
+++ detr-main/main.py
@@ -180,7 +180,8 @@ def main(args):
         #del checkpoint["model"]["class_embed.bias"]
         #del checkpoint["model"]["query_embed.weight"]
 
-        model_without_ddp.load_state_dict(checkpoint['model'], strict=False)
+        #model_without_ddp.load_state_dict(checkpoint['model'], strict=False)
+        model_without_ddp.load_state_dict(checkpoint['state_dict'], strict=False)
         if not args.eval and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
             optimizer.load_state_dict(checkpoint['optimizer'])
             lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
