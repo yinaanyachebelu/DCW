826750f5159c59f52efe611822a7238989d5b027
Ayina Anyachebelu <ayinaanyachebelu@gmail.com>
2023-08-27 02:26:20 -0400

    small edit

--- detr-main/main_test.py
+++ detr-main/main_test.py
@@ -193,7 +193,7 @@ def main(args):
         jdict = evaluate(model, criterion, postprocessors,
                          data_loader_val, base_ds, device, args.output_dir)
 
-        res = jdict[0]
+        res = jdict
         print("results")
         print(res)
 
