7f77dcb6ba72671e979e45a71e7edcd894a8800e
Ayina Anyachebelu <ayinaanyachebelu@gmail.com>
2023-08-27 00:11:06 -0400

    troubleshootoing

--- detr-main/models/detr_test.py
+++ detr-main/models/detr_test.py
@@ -273,12 +273,12 @@ class PostProcess(nn.Module):
 
         assert len(out_logits) == len(target_sizes)
         assert target_sizes.shape[1] == 2
-         
+
         prob = F.softmax(out_logits, -1)
         prob = prob.cpu().data.numpy()
         scores, labels = prob[..., :-1].max(-1)
-        scores = scores.cpu().data.numpy()
-        labels = scores.cpu().data.numpy()
+        #scores = scores.cpu().data.numpy()
+        #labels = scores.cpu().data.numpy()
 
         # convert to [x0, y0, x1, y1] format
         boxes = box_ops.box_cxcywh_to_xyxy(out_bbox)
