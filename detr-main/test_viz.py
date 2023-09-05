8121c0009ba1c64521eb165990285548f544c368
Ayina Anyachebelu <ayinaanyachebelu@gmail.com>
2023-09-04 20:08:06 -0600

    troubleshoot

--- detr-main/test_viz.py
+++ detr-main/test_viz.py
@@ -174,23 +174,24 @@ def evaluate_test(model, criterion, postprocessors, data_loader, device, thres=0
     #fig = plt.subplots(2, 4, figsize=(26, 17))
     fig = plt.figure(figsize=(26, 17))
 
-    for i, samples, targets in enumerate(data_loader):
+    for i, batch in enumerate(data_loader):
+        for targets, samples in batch:
 
-        samples = list(samp.to(device) for samp in samples)
-        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+            samples = list(samp.to(device) for samp in samples)
+            targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
-        outputs = model(samples)
+            outputs = model(samples)
 
-        orig_target_sizes = torch.stack([t["orig_size"] for t in targets], dim=0)
-        results, results_nodict = postprocessors['bbox'](outputs, orig_target_sizes)
+            orig_target_sizes = torch.stack([t["orig_size"] for t in targets], dim=0)
+            results, results_nodict = postprocessors['bbox'](outputs, orig_target_sizes)
 
         # if 'segm' in postprocessors.keys():
         #     target_sizes = torch.stack([t["size"] for t in targets], dim=0)
         #     results = postprocessors['segm'](results, outputs, orig_target_sizes, target_sizes)
 
-        res = {target['image_id'].item(): output for target, output in zip(targets, results)}
+            res = {target['image_id'].item(): output for target, output in zip(targets, results)}
 
-        plt.imshow(samples.permute(1, 2, 0))
+            plt.imshow(samples.permute(1, 2, 0))
         #ax = fig.add_subplot(2, 4, i + 1)
         # ax.imshow(samples)
         # for original_id, prediction in res.items():
