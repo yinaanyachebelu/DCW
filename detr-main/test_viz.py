d6c5b42a1571efea1441638462f5346f470e8948
Ayina Anyachebelu <ayinaanyachebelu@gmail.com>
2023-09-04 21:59:16 -0600

    troubleshooting

--- detr-main/test_viz.py
+++ detr-main/test_viz.py
@@ -22,6 +22,7 @@ from models import build_model
 import datasets
 from datasets import build_dataset, get_coco_api_from_dataset
 from datasets.weed_coco import make_Weed_transforms
+import datasets.transforms as T
 from engine_viz import evaluate, train_one_epoch
 
 import matplotlib.pyplot as plt
@@ -42,6 +43,23 @@ def rescale_bboxes(out_bbox, size):
                           img_w, img_h
                           ], dtype=torch.float32)
     return b
+
+
+def plot_results(pil_img, prob, boxes, COLORS, CLASSES):
+    plt.figure(figsize=(16, 10))
+    plt.imshow(pil_img)
+    ax = plt.gca()
+    colors = COLORS * 100
+    for p, (xmin, ymin, xmax, ymax), c in zip(prob, boxes.tolist(), colors):
+        ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                   fill=False, color=c, linewidth=3))
+        cl = p.argmax()
+        text = f'{CLASSES[cl]}: {p[cl]:0.2f}'
+        ax.text(xmin, ymin, text, fontsize=15,
+                bbox=dict(facecolor='yellow', alpha=0.5))
+    plt.axis('off')
+    plt.show()
+    plt.savefig("graphics/test_graphic.jpg")
 
 
 def get_images(in_path):
@@ -183,13 +201,48 @@ def evaluate_test(model, criterion, postprocessors, data_loader, device, thres=0
     model.eval()
     criterion.eval()
 
-    imgs = get_images("/home/ayina/MscThesis/DCW/datasets/Dataset_final/DATA_0_COCO_format/test2017/")
-    print(imgs)
+    # colors for vizualization
+    COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
+              [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
+
+    cats = [
+        "Waterhemp",
+        "MorningGlory",
+        "Purslane",
+        "SpottedSpurge",
+        "Carpetweed",
+        "Ragweed",
+        "Eclipta",
+        "PricklySida",
+        "PalmerAmaranth",
+        "Sicklepod",
+        "Goosegrass",
+        "CutleafGroundcherry"
+    ]
+
+    normalize = T.Compose([
+        T.ToTensor(),
+        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    trans = T.Compose([
+        T.RandomResize([800], max_size=1333),
+        normalize,
+    ])
+
+    url = '/home/ayina/MscThesis/DCW/datasets/Dataset_final/DATA_0_COCO_format/test2017/000000000183.jpg'
+    im = Image.open(url)
+    img = trans(im).unsqueeze(0)
+
+    probas = outputs['pred_logits'].softmax(-1)[0, :, :-1]
+    keep = probas.max(-1).values > 0.9
+    bboxes_scaled = rescale_bboxes(outputs['pred_boxes'][0, keep], im.size)
+    plot_results(im, probas[keep], bboxes_scaled, COLORS, CLASSES=cats)
+
+    #imgs = get_images("/home/ayina/MscThesis/DCW/datasets/Dataset_final/DATA_0_COCO_format/test2017/")
 
     #fig = plt.subplots(2, 4, figsize=(26, 17))
-    fig = plt.figure(figsize=(26, 17))
+    #fig = plt.figure(figsize=(26, 17))
 
-    count = 0
     for samples, targets in data_loader:
 
         samples = samples.to(device)
