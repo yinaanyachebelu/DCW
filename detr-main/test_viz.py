5cd9da007ff4ffd252ab51003bf592181ac6a2be
Ayina Anyachebelu <ayinaanyachebelu@gmail.com>
2023-09-04 21:12:58 -0600

    small edit

--- detr-main/test_viz.py
+++ detr-main/test_viz.py
@@ -166,22 +166,6 @@ def prepare_viz(predictions):
     return coco_results
 
 
-class ImageFolderWithPaths(datasets.ImageFolder):
-    """Custom dataset that includes image file paths. Extends
-    torchvision.datasets.ImageFolder
-    """
-
-    # override the __getitem__ method. this is the method that dataloader calls
-    def __getitem__(self, index):
-        # this is what ImageFolder normally returns
-        original_tuple = super(ImageFolderWithPaths, self).__getitem__(index)
-        # the image file path
-        path = self.imgs[index][0]
-        # make a new tuple that includes original and the path
-        tuple_with_path = (original_tuple + (path,))
-        return tuple_with_path
-
-
 @torch.no_grad()
 def evaluate_test(model, criterion, postprocessors, data_loader, device, thres=0.8):
     model.eval()
