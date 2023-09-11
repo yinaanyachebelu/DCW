5e0cbc547f9e9c6504043ffc0446ebe698810fbb
Ayina Anyachebelu <ayinaanyachebelu@gmail.com>
2023-09-11 11:28:06 -0400

    tb

--- deformable-detr/main.py
+++ deformable-detr/main.py
@@ -291,6 +291,8 @@ def main(args):
                 lr_scheduler.step_size = args.lr_drop
                 lr_scheduler.base_lrs = list(
                     map(lambda group: group['initial_lr'], optimizer.param_groups))
+                print("base lr:")
+                print(lr_scheduler.base_lrs)
             lr_scheduler.step(lr_scheduler.last_epoch)
             args.start_epoch = checkpoint['epoch'] + 1
         # check the resumed model
