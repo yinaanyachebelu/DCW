671ca692e1ca68c31f991f67f0177a95ca233251
Ayina Anyachebelu <ayinaanyachebelu@gmail.com>
2023-09-11 04:16:29 -0400

    troubleshoot

--- deformable-detr/attention.py
+++ deformable-detr/attention.py
@@ -227,8 +227,11 @@ def evaluate_test(model, criterion, postprocessors, data_loader, device, thres=0
         hook.remove()
 
     conv_features = conv_features[0]
-    enc_attn_weights = enc_attn_weights[0][1].cpu()
-    dec_attn_weights = dec_attn_weights[0][1].cpu()
+    enc_attn_weights = enc_attn_weights[1].cpu()
+    dec_attn_weights = dec_attn_weights[1].cpu()
+
+    enc_attn_weights = enc_attn_weights[0]
+    dec_attn_weights = dec_attn_weights[0]
 
     f_map = conv_features['0']
     print("Encoder attention:      ", enc_attn_weights[0].shape)
