ea92a43c061897cd769af92ddac98faddb1748c4
Ayina Anyachebelu <ayinaanyachebelu@gmail.com>
2023-09-11 15:54:36 -0400

    plotting last run

--- deformable-detr/plotting.py
+++ deformable-detr/plotting.py
@@ -1,11 +1,11 @@
 from util.plot_utils import plot_logs
 from pathlib import Path
 
-log_directory = [Path('runs_FOCAL2/')]
+log_directory = [Path('runs_focal_laprop/')]
 
-fields_of_interest = ('mAP','loss')
+fields_of_interest = ('mAP', 'loss')
 
 
 if __name__ == "__main__":
-    
-    plot_logs(log_directory, fields = fields_of_interest)
+
+    plot_logs(log_directory, fields=fields_of_interest)
