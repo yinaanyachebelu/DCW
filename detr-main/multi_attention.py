5abdd784f559944686b95bd90fc1ebd85ba65d60
Ayina Anyachebelu <ayinaanyachebelu@gmail.com>
2023-09-11 00:19:53 -0400

    multi head attention for image 22

--- detr-main/multi_attention.py
+++ detr-main/multi_attention.py
@@ -153,8 +153,9 @@ def evaluate_test(model, criterion, postprocessors, data_loader, device, thres=0
     thresh = 0.75
 
     img_61 = "/home/ayina/MscThesis/DCW/datasets/Dataset_final/DATA_0_COCO_format/test2017/000000000061.jpg"
+    img_61 = "/home/ayina/MscThesis/DCW/datasets/Dataset_final/DATA_0_COCO_format/test2017/000000000022.jpg"
 
-    orig_image = Image.open(img_61)
+    orig_image = Image.open(img_22)
     w, h = orig_image.size
     transform = make_Weed_transforms("test")
     dummy_target = {
@@ -207,7 +208,7 @@ def evaluate_test(model, criterion, postprocessors, data_loader, device, thres=0
 
     # taken from FB Research DETR hands-on tutorial notebook: https://colab.research.google.com/github/facebookresearch/detr/blob/colab/notebooks/detr_attention.ipynb#scrollTo=hYVZjfGhYTEa
 
-    fig, axs = plt.subplots(ncols=len(bboxes_scaled), nrows=2, figsize=(10, 10))
+    fig, axs = plt.subplots(ncols=len(bboxes_scaled), nrows=2, figsize=(22, 10))
     colors = COLORS * 100
     for idx, ax_i, (xmin, ymin, xmax, ymax) in zip(keep.nonzero(), axs.T, bboxes_scaled):
         ax = ax_i[0]
@@ -223,7 +224,7 @@ def evaluate_test(model, criterion, postprocessors, data_loader, device, thres=0
         ax.axis('off')
         ax.set_title(cats[probas[idx].argmax()])
     fig.tight_layout()
-    plt.savefig('graphics/61_att_enc.jpg')
+    plt.savefig('graphics/22_att_enc.jpg')
 
 
 def main(args):
