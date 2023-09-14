3a64be9c4d1d147289ded4b1059b21dd50615afc
Ayina Anyachebelu <ayinaanyachebelu@gmail.com>
2023-09-14 19:28:51 -0400

    small edit

--- deformable-detr/main_scratch.py
+++ deformable-detr/main_scratch.py
@@ -38,7 +38,7 @@ def get_args_parser():
                         default=['reference_points', 'sampling_offsets'], type=str, nargs='+')
     parser.add_argument('--lr_linear_proj_mult', default=1, type=float)
     parser.add_argument('--batch_size', default=4, type=int)
-    parser.add_argument('--weight_decay', default=9e-5, type=float)
+    parser.add_argument('--weight_decay', default=1e-5, type=float)
     parser.add_argument('--epochs', default=35, type=int)
     parser.add_argument('--lr_drop', default=50, type=int)
     parser.add_argument('--lr_drop_epochs', default=None, type=int, nargs='+')
