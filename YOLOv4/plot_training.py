be613a171acc5f0fa2115620e6624f6b714ebd54
Ayina Anyachebelu <ayinaanyachebelu@gmail.com>
2023-09-16 19:20:35 -0400

    plot_trianing for yolo added

---
+++ YOLOv4/plot_training.py
@@ -0,0 +1,8 @@
+from utils.general import *; plot_results_overlay()
+
+if __name__ == '__main__':
+    #parser = argparse.ArgumentParser(
+        #'Deformable DETR training and evaluation script', parents=[get_args_parser()])
+    #args = parser.parse_args()
+    #main(args)
+    plot_results_overlay()
